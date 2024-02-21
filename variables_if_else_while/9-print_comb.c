1a13e045808c78d9f94133cfb402809f85e6fda5
cyberkity888 <anadanielamore76@gmail.com>
2024-02-21 14:17:58 -0400

    printing 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nums;
+
+	for (nums = 0; nums <= 9; nums++)
+{
+	putchar((nums % 10) + '0');
+
+	if (nums < 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
