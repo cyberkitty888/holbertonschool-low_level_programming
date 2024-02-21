fab1e841f725fa4b1083c144e29bb46ad307e635
cyberkity888 <anadanielamore76@gmail.com>
2024-02-21 14:13:49 -0400

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - acces point
+*description: prints num 0 to 9
+*Return: always (0)
+*/
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+printf("%d", n);
+printf("\n");
+return (0);
+}
