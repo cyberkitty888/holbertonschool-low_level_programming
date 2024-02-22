d1ea0471b9d9f879f338cbfc1cb7a31c2cbf922a
cyberkity888 <anadanielamore76@gmail.com>
2024-02-21 20:39:57 -0400

    Alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - alphabet in lowecase
+*Return: Always (0)
+*/
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+
+return (0);
+}
