f26027b1dc106eb04ee34c02d6b97dbcd41c5158
cyberkity888 <anadanielamore76@gmail.com>
2024-02-22 10:06:09 -0400

    Aplhabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
-*main - alphabet in lowecase
-*Return: Always (0)
-*/
-int main(void)
+ * print_alphabet - This is the function of the task
+ * Return: return 0
+ */
+
+void print_alphabet(void)
 {
-char c;
 
-for (c = 'a'; c <= 'z'; c++)
-putchar(c);
-putchar('\n');
-
-return (0);
+ char c;
+ for (c = 'a'; c <= 'z'; c++)
+ {
+_putchar(c);
+ }
+_putchar('\n');
 }
