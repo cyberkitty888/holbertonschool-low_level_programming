43e94a1f49e6c1f3aa0bfb8582bee59407a6822d
cyberkity888 <anadanielamore76@gmail.com>
2024-02-22 10:50:20 -0400

    Alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10
+ *
+ * Description: using _putchar to print
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j< 10; j++)
+	{
+                for (i = 'a'; i <= 'z'; i++)
+	}
+	_putchar('\n');
+}
