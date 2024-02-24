6263619a56a44f5dbb6bd60ff783fd949bb189ce
cyberkity888 <anadanielamore76@gmail.com>
2024-02-24 19:38:54 -0400

    Modified answer

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,27 +1,13 @@
 #include "main.h"
 /**
- * _isalpha - return 1 when c is cap or low 
- *
- * Description: we are using _putchar
- *
- * @c: collect the character
- *
- * Return: 1 if c is low or cap otherwise 0
- */
+*_isalpha - controls if a character is alphabetical
+*@c: character to be verified
+*Return: return 0 or 1
+*/
 int _isalpha(int c)
-{	
-	char lower, upper;
-	int letter = 0;
+{
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+return (1);
 
-	for (lower = 'a'; lower <= 'z'; lower++)
-	{
-		for (upper = 'A'; upper <= 'Z'; upper++)
-		{
-			if (lower == c || upper == c)
-			{
-				letter = 1;
-			}
-		}
-	}
-	return (letter);
+return (0);
 }
